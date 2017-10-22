e7060ff84b7836bc2de9c2470868baa54be8199b
bk8335 <bk8335@gmail.com>
2017-10-22 09:01:38 +0100

    quiz page with styling

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -10,4 +10,7 @@ class StaticController < ApplicationController
 
   def about
   end
+
+  def quiz
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/lessons', to: 'static#lessons'
   get '/about', to: 'static#about'
   get '/contact', to: 'static#contact'
+  get '/quiz', to: 'static#quiz'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
