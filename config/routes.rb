16c1b3f604eb39c375df4a5fd903433cd875a62d
bk8335 <bk8335@gmail.com>
2017-10-29 08:54:39 +0000

    changes as requested from Nik 29 October

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get '/about', to: 'static#about'
   get '/contact', to: 'static#contact'
   get '/quiz', to: 'static#quiz'
+  get '/confidence_kickstarter', to: 'static#confidence_kickstarter'
+  get '/equine_first_aid_courses', to: 'static#equine_first_aid_courses'
+  get '/success_stories', to: 'static#success_stories'
+  get '/free_resources', to: 'static#free_resources'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
