74259a4171402fa677387b17a601c11d38dc352a
bk8335 <bk8335@gmail.com>
2018-03-03 12:49:18 +0000

    form and photos in correct order

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,6 @@
 class StaticController < ApplicationController
   def home
+    @newsletter_subscriber = NewsletterSubscriber.new
   end
 
   def lessons
@@ -12,5 +13,22 @@ class StaticController < ApplicationController
   end
 
   def quiz
+  end
+
+  def newsletter_sign_up
+    @newsletter_subscriber = NewsletterSubscriber.new(newsletter_subscriber_params)
+    if @newsletter_subscriber.save
+      flash[:success] = "Thanks for signing up - we'll be in touch soon with the latest from NKC Equestrian Training!"
+      redirect_to root_url
+    else
+      flash[:error] = "Sorry, please make sure you submit a valid name and email address"
+      redirect_to root_url
+    end
+  end
+
+  private
+
+  def newsletter_subscriber_params
+    params.require(:newsletter_subscriber).permit(:name, :email)
   end
 end
