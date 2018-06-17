6ac4003a725f5da7ee1fab0b737106863790723f
bk8335 <bk8335@gmail.com>
2018-06-17 11:39:03 +0100

    images without s3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180615131145) do
+ActiveRecord::Schema.define(version: 20180617102850) do
 
   create_table "newsletter_subscribers", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20180615131145) do
     t.boolean "draft"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
