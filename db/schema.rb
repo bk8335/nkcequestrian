f547f9feedf1098e50bc936a05096805c0411851
bk8335 <bk8335@gmail.com>
2018-06-17 09:41:53 +0100

    posts model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180303110501) do
+ActiveRecord::Schema.define(version: 20180615131145) do
 
   create_table "newsletter_subscribers", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "wordcount"
+    t.boolean "draft"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
